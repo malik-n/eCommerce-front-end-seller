531217bb8138e90ba040a148b495626d74bd7084
Naeem Malik <malik2015naeem@gmail.com>
2023-12-24 23:57:03 +0530

    token validation added

--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,8 @@ import {
   from,
 } from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
+import { setContext } from "@apollo/client/link/context";
+
 import Layout from "./components/Layout/index.jsx";
 
 const router = createBrowserRouter([
@@ -58,10 +60,26 @@ const link = from([
   new HttpLink({ uri: "http://localhost:3001/graphql" }),
 ]);
 
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: link,
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
 });
+
+const client = new ApolloClient({
+  link: authLink.concat(link),
+  cache: new InMemoryCache(),
+});
+
+// const client = new ApolloClient({
+//   cache: new InMemoryCache(),
+//   link: link,
+// });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
